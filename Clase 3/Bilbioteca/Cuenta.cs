cf61a7e32d9ef8aab4ee3d5584e0206cb441054f
LucianoPezza <47321345+LucianoPezza@users.noreply.github.com>
2021-08-30 22:34:34 -0300

    Add files via upload

---
+++ Clase 3/Bilbioteca/Cuenta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Bilbioteca
+{
+    public class Cuenta
+    {
+       private string titular;
+       private decimal cantidad;
+
+        public Cuenta(string titular, decimal cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+        public string GetTitular()
+        {
+            return titular;
+        }
+        public decimal GetCantidad( )
+        {
+            return cantidad;
+        }
+        
+        public string Mostrar()
+        {
+            return $"Titular: {GetTitular()}, Cantidad: {GetCantidad()}";
+        }
+
+        public void Ingresar(decimal monto)
+        {
+            if(monto >0)
+            {
+                this.cantidad += monto;
+            }
+        }
+        public void Retirar(decimal monto)
+        {
+            if (monto > 0)
+            {
+                this.cantidad -= monto;
+            }
+        }
+    }
+}
