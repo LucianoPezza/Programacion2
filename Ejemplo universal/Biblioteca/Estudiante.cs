f3b002cd42c31afbddf44a5486efb329113b715c
LucianoPezza <47321345+LucianoPezza@users.noreply.github.com>
2021-08-30 22:35:10 -0300

    Add files via upload

---
+++ Ejemplo universal/Biblioteca/Estudiante.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using Biblioteca;
+
+namespace Biblioteca
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        public Estudiante()
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public float CalcularPromedio()
+        {
+            return (notaPrimerParcial + notaSegundoParcial) / 2;
+        }
+        public double CalcularNotaFinal()
+        {
+            if (notaPrimerParcial >= 4 && notaSegundoParcial >= 4)
+            {
+                return random.Next(6, 11);
+            }
+            return -1;
+        }
+
+        public void SetNotaPrimerParcial(int notaPrimerParcial)
+        {
+            this.notaPrimerParcial = notaPrimerParcial;
+        }
+        public void SetNotaSegundoParcial(int notaSegundoParcial)
+        {
+            this.notaSegundoParcial = notaSegundoParcial;
+        }
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            double notaFinal = CalcularNotaFinal();
+            sb.AppendLine($"Nombre:{nombre}, Apellido: {apellido}, Legajo:{legajo}");
+            sb.AppendLine($"Nombre:{nombre}, Apellido: {apellido}");
+
+            if (notaFinal != -1)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Ejemplo universal/Ejemplo universal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+using Biblioteca;
+namespace Ejemplo_universal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante e1 = new Estudiante("Galarza","1","Lautaro");
+        }
+    }
+}
